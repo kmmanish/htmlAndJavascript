3ca122bdfd5931b214bdc68a18c206950a6f887b
kmmanish <84233822+kmmanish@users.noreply.github.com>
2021-12-11 10:32:23 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 
+import {findingMaxNumber} from "./module/manish.js";
+
+console.log('main file');
+
+let mylist = [99,9,14, -87, 450, 320, 67, 136];
+
+let max = 0;
+
+max = findingMaxNumber(mylist);
+
+console.log(max);
